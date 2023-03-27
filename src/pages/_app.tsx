c8058590192f1cf124b1056be5c723cca5cc74ca
solty <>
2023-03-28 00:56:19 +0900

    no message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 const MyApp: NextPage<AppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) => {
+  useEffect(() => {
+    const login = async () => {
+      const liff = await import('@line/liff');
+      await liff.default.init({ liffId: '1660794789-0qKJO77l' });
+      if (!liff.default.isLoggedIn()) {
+        liff.default.login();
+      }
+
+      const profile = await liff.default.getProfile();
+      profile.displayName;
+      console.log(profile.displayName);
+    };
+    login();
+  }, []);
+
   return (
     <>
       {/* <Head>

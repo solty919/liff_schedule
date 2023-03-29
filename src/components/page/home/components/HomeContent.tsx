c6f877f323768c10dd5e9789f63e2ea78c52cd4f
solty <>
2023-03-29 21:26:08 +0900

    no message

--- src/components/page/home/components/HomeContent.tsx
+++ src/components/page/home/components/HomeContent.tsx
@@ -1,5 +1,22 @@
 import { HomePageProps } from '@/components/page/home/hooks/useHomePageHook';
+import { useLiff } from '@/hooks/useLiff';
+import { useEffect, useState } from 'react';
+import { Liff } from '@line/liff/';
 
 export const HomeContent = ({}: HomePageProps) => {
-  return <div>aa</div>;
+  const { liff } = useLiff();
+  const [profile, setProfile] = useState<any>();
+
+  useEffect(() => {
+    if (!liff) return;
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+    liff.getProfile().then((profile) => {
+      console.log(profile);
+      setProfile(profile);
+    });
+  });
+
+  return <div>v1 aa{profile}</div>;
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { LiffProvider } from '@/hooks/useLiff';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
@@ -6,30 +7,10 @@ const MyApp: NextPage<AppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) => {
-  useEffect(() => {
-    const login = async () => {
-      const liff = await import('@line/liff');
-      await liff.default.init({ liffId: '1660794789-0qKJO77l' });
-      if (!liff.default.isLoggedIn()) {
-        liff.default.login();
-      }
-
-      const profile = await liff.default.getProfile();
-      profile.displayName;
-      console.log(profile.displayName);
-    };
-    login();
-  }, []);
-
   return (
-    <>
-      {/* <Head>
-        <title>在庫管理</title>
-      </Head> */}
-      {/* <ChakraProvider> */}
+    <LiffProvider liffId="1660794789-0qKJO77l">
       <Component {...pageProps} />
-      {/* </ChakraProvider> */}
-    </>
+    </LiffProvider>
   );
 };
 
